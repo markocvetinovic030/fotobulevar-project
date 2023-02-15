f9c734b7006cd040f233ffb2e7df64022c69e835
Marko Cvetinovic <markocvetinovic030@gmail.com>
2023-02-15 16:08:42 +0100

    promenjena pocetna

--- js/script.js
+++ js/script.js
@@ -2,10 +2,28 @@ let header = document.querySelector('header');
 
 window.onscroll = function() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      header.style.backgroundColor = "#fff";
-      header.style.boxShadow = 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
+      header.style.backgroundColor = "#F3F3F3";
+      header.style.boxShadow = 'rgba(0, 0, 0, 0.35) 5px 5px 15px'
+      header.style.transition = '0.5s all'
     } else {
       header.style.backgroundColor = "transparent";
       header.style.boxShadow = 'none';
     }
   };
+
+
+ let nav = document.querySelector('.right')
+ let hamburger = document.querySelector('#hamburger')
+
+ hamburger.addEventListener('click', function(){
+   if (nav.style.display === "none") {
+     nav.style.display = "block";
+     header.style.backgroundColor = '#F3F3F3';
+   } else {
+     nav.style.display = "none";
+     header.style.backgroundColor = "transparent";
+   }
+ });
+  
+
+  
